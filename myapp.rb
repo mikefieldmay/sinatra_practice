03a08d4cb3a603f75bc68f2686e091190484c0b1
Mike Field-May <mfieldmay@gmail.com>
2017-01-17 12:30:31 +0000

    Addded name submission for cat

--- myapp.rb
+++ myapp.rb
@@ -12,6 +12,13 @@ get '/party' do
   "PARTY PAGE!!!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ['Amigo', 'Oscar', 'Viking'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
