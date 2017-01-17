d6af81c07dd59651a8aafd4b35737f195756bb64
Mike Field-May <mfieldmay@gmail.com>
2017-01-17 11:08:40 +0000

    Updated cat pic

--- myapp.rb
+++ myapp.rb
@@ -13,7 +13,5 @@ get '/party' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+    "<img src='http://bit.ly/1eze8aE', style='border: 3px dashed red'>"
 end
